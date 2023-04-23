84e5750ae7af4363f882526897a04c632c4fae9c
Stefan Erceg <stefanerceg95@gmail.com>
2023-04-23 22:02:51 +0200

    add cors on socket.io server

--- src/socket.ts
+++ src/socket.ts
@@ -4,10 +4,16 @@ import { Server } from 'socket.io';
 dotenv.config();
 
 const PORT: number = +(process.env.SOCKET_PORT || 0);
+const ORIGIN: string = process.env.SOCKET_CORS_ORIGIN || '';
 
 const startSocketServer = () => {
 	// create a Socket.io instance
-	const io = new Server(PORT);
+	const io = new Server(PORT, {
+		cors: {
+			origin: ORIGIN,
+			methods: ['GET', 'POST'],
+		},
+	});
 
 	// set up Socket.io event handlers
 	io.on('connection', (socket) => {
